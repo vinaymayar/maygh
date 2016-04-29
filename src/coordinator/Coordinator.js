89431ea8360dca73f1a8adc1ea30f78e5c5b0a9b
Anying Li <anyingl@mit.edu>
2016-04-28 21:39:32 -0400

    commented coordinator code

--- src/coordinator/Coordinator.js
+++ src/coordinator/Coordinator.js
@@ -1,9 +1,26 @@
+/**
+ * Maygh coordinator class.
+ * Stores important state and supports coordinator-specific functions.
+ * @author: ?
+ */
+
 function Coordinator() {
+  // map from content hashes to lists of client 
+  // pids that have that content
   this.contentToClientMap = {};
+  // inverse map from above. 
+  // map from client pids to lists of 
+  // content hashes stored by that client
   this.clientToContentMap = {};
+  // map from client pids to dictionary of information
+  // about that client
+  // Ex: {1: {IP: 18.xxx.xxx.xxx, etc.}}
   this.clientsInfoMap = {};
 }
 
+// Looks up and returns a client that has the 
+// specified content.
+// Returns null if no client has that content
 Coordinator.prototype.lookup = function(contentHash) {
   clients = this.contentToClientMap[contentHash]
   if (clients == null || clients.length == 0) {
@@ -12,6 +29,7 @@ Coordinator.prototype.lookup = function(contentHash) {
   return clients[0]
 };
 
+// Adds a client to the clientsInfoMap with given clientInfo.
 Coordinator.prototype.addClient = function(clientPID, clientInfo) {
   // TODO: check if client already exists
   this.clientsInfoMap[clientPID] = clientInfo
--- src/coordinator/server.js
+++ src/coordinator/server.js
@@ -1,5 +1,13 @@
+/**
+ * Maygh coordinator server.
+ * Handles all the communication with clients.
+ * @author: Anying Li, Lara Araujo, Vinay Mayar
+ */
+
 var Coordinator = require('./Coordinator.js').Coordinator
 
+// Note: keeping the http server for now just for ease of
+// manual testing.
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
@@ -9,21 +17,21 @@ var COORDINATOR_PORT = 8000;
 var coordinator = new Coordinator()
 
 app.listen(COORDINATOR_PORT);
-
 function handler (req, res) {
   res.end('Path Hit: ' + req.url);
 }
 
+// The Coordinator and Client communicate through the following messages:
+//    'initiate': Sent on connection from client. Should add that client's
+//                information to the coordinator state
+//    'lookup': Looks up a client that has the specified content.
 io.on('connection', function (socket) {
+  // data: {}
   socket.on('initiate', function (data) {
-    /*
-    Data: IP, etc.
-    Generates a pid for that client, and then creates
-    a room.
-    */
     var pid = socket.id
     coordinator.addClient(pid, data)
   });
+  // data: { 'content_hash': XXXXXX }
   socket.on('lookup', function (data, callback) {
     console.log("Server received lookup message")
     pid = coordinator.lookup(data['content_hash'])
