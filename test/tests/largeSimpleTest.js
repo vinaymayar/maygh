33aa8caed4663f537a3625333ae5867ce4b59af3
Vinay Mayar <vinaymayar@gmail.com>
2016-05-11 15:49:49 -0400

    Fix semicolons in tests

--- test/tests/largeSimpleTest.js
+++ test/tests/largeSimpleTest.js
@@ -7,15 +7,15 @@ module.exports = new (function() {
       client
         .url('http://localhost:8080/')
         .waitForElementVisible('body', 1000)
-        .expect.element('#image2').to.have.attribute('src').after(5000)
+        .expect.element('#image2').to.have.attribute('src').after(4000);
       client
         .expect.element('#image2').to.have.attribute('data-source')
-        .which.equals('server').after(1000)
+        .which.equals('server').after(100);
     }
   }
 
   tests.after = function(client) {
-    client.end()
+    client.end();
   }
 
 })();
