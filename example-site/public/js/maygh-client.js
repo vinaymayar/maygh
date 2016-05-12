2206c12f9ca8fab04ac7bab2ac9782a4e34d8158
Vinay Mayar <vinaymayar@gmail.com>
2016-05-11 20:18:47 -0400

    Add maygh-loaded js file to example site

--- example-site/public/js/maygh-client.js
+++ example-site/public/js/maygh-client.js
@@ -185,9 +185,9 @@ function loadFromSrc(contentHash, src, domElt) {
       localStorage.setItem(contentHash, datauri);
       setDomEltContent(domElt, datauri);
       domElt.setAttribute('data-source', 'server');
+
       console.log("HASH IS");
       console.log(Sha1.hash(datauri));
-
       // sends a update message telling the server, we have the element
       // in out local storage
       maygh.socket.emit('update', {'contentHash': contentHash, 'pid': maygh.socket.id});
--- src/client/client.js
+++ src/client/client.js
@@ -185,8 +185,6 @@ function loadFromSrc(contentHash, src, domElt) {
       localStorage.setItem(contentHash, datauri);
       setDomEltContent(domElt, datauri);
       domElt.setAttribute('data-source', 'server');
-      console.log("HASH IS");
-      console.log(Sha1.hash(datauri));
 
       // sends a update message telling the server, we have the element
       // in out local storage
